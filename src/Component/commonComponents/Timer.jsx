5a7421aacc5487a5e6d143bd02950dc876739b55
Tushar <153347682+Web-Tushar@users.noreply.github.com>
2024-11-15 22:59:11 +0600

    Cannot access 'worker' before initialization

--- src/Component/commonComponents/Timer.jsx
+++ src/Component/commonComponents/Timer.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'  
 
 const Timer = () => {
+     const [time, setTime] = useState(3*24*60*60*1000)
+     useEffect (()=>{
+           const worker = new worker(('../../CountDownWorker.js', import.meta.url))
+           worker.postmessage(time)
+           worker.onmessage=(e)=>{
+               setTime(e.data);
+           };
+     },[])
+
+     // useEffect(()=>{
+     //      setTimeout(() => {
+     //           setTime(time - 1000)
+     //      }, 1000);
+     // }, [time])
+     const formateDate = (millisecond)=>{
+          let total_second = parseInt(Math.floor(millisecond /1000 ));
+          let total_minutes = parseInt(Math.floor(total_second /60 ));
+          let total_hours = parseInt(Math.floor(total_minutes /60 ));
+          let Days = parseInt(Math.floor(total_hours /24 ));
+          let second = parseInt(Math.floor(total_second % 60 ));
+          let minutes = parseInt(Math.floor(total_minutes % 60 ));
+          let hours = parseInt(Math.floor(total_hours  % 60));
+          return {Days,second, minutes, hours };
+
+     }
+     const {Days, hours,minutes, second } = formateDate(time)
   return (
-    <div>Timer</div>
+       <div className='container'>
+     <div className='flex items-center  gap-x-[17px]'>
+
+         <div>
+              <span className='font-poppins text-[12px] font-medium text-text_000000'>Days</span>
+              <h2 className='text-text_000000 font-Inter text-[32px] font-bold	'>
+               {Days <10 && `0 ${Days}`}
+
+              </h2>
+         </div>
+         <span className='text-red-700 text-[40px]'>:</span>
+         <div>
+              <span className='font-poppins text-[12px] font-medium text-text_000000'>Hours</span>
+              <h2 className='text-text_000000 font-Inter text-[32px] font-bold	'>{hours && hours}</h2>
+         </div>
+         <span className='text-red-700 text-[40px]'>:</span>
+
+         <div>
+              <span className='font-poppins text-[12px] font-medium text-text_000000'>Minutes</span>
+              <h2 className='text-text_000000 font-Inter text-[32px] font-bold	'>{minutes && minutes}</h2>
+         </div>
+         <span className='text-red-700 text-[40px]'>:</span>
+
+         <div>
+              <span className='font-poppins text-[12px] font-medium text-text_000000'>Seconds</span>
+              <h2 className='text-text_000000 font-Inter text-[32px] font-bold	'>{second && second}</h2>
+         </div>
+        </div>
+     </div>
   )
 }
 
--- src/Component/homePage/flash sale/FlashSale.jsx
+++ src/Component/homePage/flash sale/FlashSale.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Heading from '../../commonComponents/Heading'
+import Timer from '../../commonComponents/Timer'
 
 const FlashSale = () => {
   return (
-    <div  className='mt-[140px] mb-[60px]'>
-      <Heading  title={"Today’s" } description={"Flash Sales"}/>
+    <div className='mt-[140px] mb-[60px]'>
+      <div className='container  gap-x-[87px] flex items-end'>
+        <div >
+          <Heading title={"Today’s"} description={"Flash Sales"} />
+        </div> 
+        <div><Timer /></div>
+      </div>
     </div>
+
   )
 }
 
