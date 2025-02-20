26afa2c4e1c85de22cfd7d174bad31167e66d592
Tushar <153347682+Web-Tushar@users.noreply.github.com>
2025-02-20 23:03:56 +0600

    login Link add in sign up page

--- src/pages/sign up/Signup.jsx
+++ src/pages/sign up/Signup.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
      return (
@@ -107,22 +108,23 @@ const Signup = () => {
                                    {/* <!-- Your form elements go here --> */}
                                    <div>
                                         <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
-                                        <input type="text" id="username" name="username" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"/>
+                                        <input type="text" id="username" name="username" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300" />
                                    </div>
                                    <div>
                                         <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-                                        <input type="text" id="email" name="email" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"/>
+                                        <input type="text" id="email" name="email" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300" />
                                    </div>
                                    <div>
                                         <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-                                        <input type="password" id="password" name="password" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"/>
+                                        <input type="password" id="password" name="password" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300" />
                                    </div>
                                    <div>
                                         <button type="submit" class="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800  focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">Sign Up</button>
                                    </div>
                               </form>
                               <div class="mt-4 text-sm text-gray-600 text-center">
-                                   <p>Already have an account? <a href="#" class="text-black hover:underline">Login here</a>
+                                   <p>Already have an account?
+                                        <Link to={"/Login"} class="text-black hover:underline">Login here</Link>
                                    </p>
                               </div>
                          </div>
